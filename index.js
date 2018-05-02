df779b9e40868b1bbf12f3cce3c6fa7b39dc0fc3
Paolo Carando <paolo.carando@xurpas.com>
2018-05-02 16:07:26 +0800

    Add css, js files

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var ctx =  $('#canvas')[0].getContext('2d');
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.canvas.style = 'vertical-align: bottom;';
+    var count = 0;
+    var audio = new Audio('audio.wav');
+    var img = new Image();
+    img.src = 'image.png';
+    img.onload = function() {
+        $('#canvas').on('click', function(e) {
+            var x = Math.round(e.offsetX - (img.width / 2));
+            var y = Math.round(e.offsetY - (img.height / 2));
+            ctx.drawImage(img, x, y);
+            audio.currentTime = 0;
+            audio.play();
+            count = count + 1;
+            $('#count').html(count);
+            if (count == 23) {
+                $('#center').addClass('happy-birthday');
+                $('#center').html('happy birthday, beth!');
+            }
+        });
+    };
+});

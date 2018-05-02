262b003fa6a23219be3bb8e0ed71b28675674586
Paolo Carando <paolo.carando@xurpas.com>
2018-05-02 19:15:35 +0800

    Multiple media

--- index.js
+++ index.js
@@ -2,30 +2,46 @@ $(document).ready(function() {
     var ctx =  $('#canvas')[0].getContext('2d');
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    ctx.canvas.style = 'vertical-align: bottom;';
+    ctx.canvas.style = 'vertical-align: bottom;';    
     var count = 0;
-    var audio = new Audio('audio.wav');
-    var img = new Image();
-    img.src = 'image.png';
-    img.onload = function() {
-        $('#canvas').on('click', function(e) {
-            var x = Math.round(e.offsetX - (img.width / 2));
-            var y = Math.round(e.offsetY - (img.height / 2));
-            ctx.drawImage(img, x, y);
-            audio.currentTime = 0;
-            audio.play();
-            count = count + 1;
-            $('#count').html(count);
-            if (count >= 23) {
-                $('#center').addClass('happiest-birthday');
-                $('#center').html('happiest birthday, beth!');
-            } else if (count >= 13) {
-                $('#center').addClass('happier-birthday');
-                $('#center').html('happier birthday, beth!');
-            } else if (count >= 3) {
-                $('#center').addClass('happy-birthday');
-                $('#center').html('happy birthday, beth!');
-            }
-        });
-    };
+    var audio1 = new Audio('audio1.wav');
+    var audio2 = new Audio('audio2.wav');
+    var audio3 = new Audio('audio3.wav');
+    var image1 = new Image();
+    image1.src = 'image1.png';
+    var image2 = new Image();
+    image2.src = 'image2.png';
+    var image3 = new Image();
+    image3.src = 'image3.png';
+    $('#canvas').on('click', function(e) {
+        count = count + 1;
+        $('#count').html(count);
+        var audio = new Audio();
+        var image = new Image();
+        var centerClass = '';
+        var centerHtml = '';
+        if (count >= 23) {
+            audio = audio3;
+            image = image3;
+            centerClass = 'happiest-birthday';
+            centerHtml = 'happiest birthday, beth!';
+        } else if (count >= 13) {
+            audio = audio2;
+            image = image2;
+            centerClass = 'happier-birthday';
+            centerHtml = 'happier birthday, beth!';
+        } else {
+            audio = audio1;
+            image = image1;
+            centerClass = 'happy-birthday';
+            centerHtml = 'happy birthday, beth!';
+        }
+        audio.currentTime = 0;
+        audio.play();
+        var x = Math.round(e.offsetX - (image.width / 2));
+        var y = Math.round(e.offsetY - (image.height / 2));
+        ctx.drawImage(image, x, y);
+        $('#center').addClass(centerClass);
+        $('#center').html(centerHtml);
+    });
 });

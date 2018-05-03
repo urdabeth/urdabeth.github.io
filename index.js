fabe45d95ba0372cdb9a85bde6b137a4bde1e196
user <carandopc@gmail.com>
2018-05-03 21:08:38 +0800

    One audio, random images

--- index.js
+++ index.js
@@ -4,36 +4,30 @@ $(document).ready(function() {
 	ctx.canvas.height = window.innerHeight;
 	ctx.canvas.style = 'vertical-align: bottom;';    
 	var count = 0;
-	var audio1 = new Audio('audio1.wav');
-	var audio2 = new Audio('audio2.wav');
-	var audio3 = new Audio('audio3.wav');
-	var image1 = new Image();
-	image1.src = 'image1.png';
-	var image2 = new Image();
-	image2.src = 'image2.png';
-	var image3 = new Image();
-	image3.src = 'image3.png';
+	var audio = new Audio('audio.wav');
+	var images = new Array();
+	images[0] = new Image();
+	images[0].src = 'image1.png';
+	images[1] = new Image();
+	images[1].src = 'image2.png';
+	images[2] = new Image();
+	images[2].src = 'image3.png';
 	var centerClass = '';
 	var centerText = '';
 	var subcenterClass = 'hidden';
 	$('#canvas').on('click', function(e) {
-		var audio = new Audio();
 		var image = new Image();
+		var random = Math.floor(Math.random() * 3);
+		image = images[random];
 		count = count + 1;
 		if (count > 22) {
-			audio = audio3;
-			image = image3;
 			centerClass = 'happiest-birthday';
 			centerText = 'happiest birthday, beth!';
 			subcenterClass = 'shown';
 		} else if (count > 12) {
-			audio = audio2;
-			image = image2;
 			centerClass = 'happier-birthday';
 			centerText = 'happier birthday, beth!';
 		} else {
-			audio = audio1;
-			image = image1;
 			centerClass = 'happy-birthday';
 			centerText = 'happy birthday, beth!';
 		} 

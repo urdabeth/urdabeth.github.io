43ec858551c1b4178d36bf1b58b5ba901404395d
user <carandopc@gmail.com>
2018-05-03 21:38:37 +0800

    Refactor

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 	ctx.canvas.height = window.innerHeight;
 	ctx.canvas.style = 'vertical-align: bottom;';    
 	var count = 0;
+	var centerText = ['happy birthday, beth!', 'happier birthday, beth!','happiest birthday, beth!'];
+	var centerClass = ['happy-birthday','happier-birthday', 'happiest-birthday'];
+	var subcenterClass = ['hidden', 'hidden', 'shown'];
 	var audio = new Audio('audio.wav');
 	var images = new Array();
 	images[0] = new Image();
@@ -12,35 +15,21 @@ $(document).ready(function() {
 	images[1].src = 'image2.png';
 	images[2] = new Image();
 	images[2].src = 'image3.png';
-	var centerClass = '';
-	var centerText = '';
-	var subcenterClass = 'hidden';
 	$('#canvas').on('click', function(e) {
-		var image = new Image();
-		var random = Math.floor(Math.random() * 3);
-		image = images[random];
-		count = count + 1;
-		if (count > 22) {
-			centerClass = 'happiest-birthday';
-			centerText = 'happiest birthday, beth!';
-			subcenterClass = 'shown';
-		} else if (count > 12) {
-			centerClass = 'happier-birthday';
-			centerText = 'happier birthday, beth!';
-		} else {
-			centerClass = 'happy-birthday';
-			centerText = 'happy birthday, beth!';
-		} 
-		if (count < 24) {
+		if (count < 23) {
+			count = count + 1;
 			$('#count').text(count);
+			var cat = Math.floor(count / (23 / 2));
+			$('#centerText').text(centerText[cat]);
+			$('#centerText').attr('class', centerClass[cat]);
+			$('#subcenter').attr('class', subcenterClass[cat]);
 			audio.currentTime = 0;
 			audio.play();
+			var random = Math.floor(Math.random() * 3);
+			var image = images[random];
 			var x = Math.round(e.offsetX - (image.width / 2));
 			var y = Math.round(e.offsetY - (image.height / 2));
 			ctx.drawImage(image, x, y);
-			$('#centerText').attr('class', centerClass);
-			$('#centerText').text(centerText);
-			$('#subcenter').attr('class', subcenterClass);
 		}
 	});
 });

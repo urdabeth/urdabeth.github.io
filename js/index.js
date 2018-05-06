3a139f0aec4cad57e6d6a517334f1a5a8a66a8bc
urdabeth <carandopc@gmail.com>
2018-05-06 18:11:10 +0800

    Add assets

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 const audio = new Audio('./assets/audio/audio.wav');	
-const images = new Array();
+const images = [];
 images[0] = './assets/images/image0.png';
-images[1] = './assets/images/1.png';
-images[2] = './assets/images/2.png';
-images[3] = './assets/images/3.png';
-images[4] = './assets/images/4.png';
-images[5] = './assets/images/5.png';
-images[6] = './assets/images/6.png';
-images[7] = './assets/images/7.png';
-images[8] = './assets/images/8.png';
-images[9] = './assets/images/9.png';
-const greetings = new Array();
+images[1] = './assets/images/image1.png';
+images[2] = './assets/images/image2.png';
+images[3] = './assets/images/image3.png';
+images[4] = './assets/images/image4.png';
+images[5] = './assets/images/image5.png';
+images[6] = './assets/images/image6.png';
+images[7] = './assets/images/image7.png';
+images[8] = './assets/images/image8.png';
+images[9] = './assets/images/image9.png';
+const greetings = []
 greetings[0] = 'happy bark day';
 greetings[1] = 'i wuff you';
 greetings[2] = 'stay pawesome';
@@ -24,27 +24,26 @@ greetings[9] = 'ur a v v good one';
 $(document).ready(function() {
 	var index = 0;
 	var interval;
-	interval = setInterval(function() {
-		changeImages();
-	}, 1000);
+	setIntervalImages();
 	$('.center').on('click', function() {
 		audio.currentTime = 0;
 		audio.play();		
 		clearInterval(interval);
-		$('#greeting').text(greetings[index]);
+		$('#greeting').text(greetings[index - 1]);
 		$('.bubble').slideDown(500, function() {
 			setTimeout(function() {
 				$('.bubble').slideUp(500);				
-				interval = setInterval(function() {
-					changeImages();
-				}, 1000);
+				setIntervalImages();
 			}, 1000)
 		});
-	});
-
+	})
+	function setIntervalImages() {
+		interval = setInterval(function() {
+			changeImages();
+		}, 1000);
+	}
 	function changeImages() {
 		$('#image').attr('src', images[index]);	
 		index = (index == 9) ? 0 : (index + 1);
 	}
 });
-

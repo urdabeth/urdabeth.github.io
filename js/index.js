c9095710b5dfa888fbb52da43cd6a038a804ad28
Paolo Carando <paolo.carando@xurpas.com>
2018-05-07 10:19:38 +0800

    Preload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
-const audio = new Audio('./assets/audio/audio.wav');	
-const images = [];
-images[0] = './assets/images/image0.png';
-images[1] = './assets/images/image1.png';
-images[2] = './assets/images/image2.png';
-images[3] = './assets/images/image3.png';
-images[4] = './assets/images/image4.png';
-images[5] = './assets/images/image5.png';
-images[6] = './assets/images/image6.png';
-images[7] = './assets/images/image7.png';
-images[8] = './assets/images/image8.png';
-images[9] = './assets/images/image9.png';
+const audio = new Audio('./assets/audio/audio.wav');
 const greetings = []
 greetings[0] = 'happy bark day';
 greetings[1] = 'i wuff you';
@@ -21,16 +10,21 @@ greetings[6] = 'very celebrate';
 greetings[7] = 'so cake';
 greetings[8] = 'much age';
 greetings[9] = 'ur v v good boye';
+const images = [];
+for (var i = 0; i < 10; i++) {
+	images[i] = new Image();
+	images[i].src = './assets/images/image' + i + '.png';
+}
 $(document).ready(function() {
 	var index = 0;
 	$('.message').on('click', function() {
 		audio.currentTime = 0;
-		audio.play();	
-		$('.media').fadeOut(300, function() {
+		audio.play();
+		$('.media').fadeOut(500, function() {
 			$('#greeting').text(greetings[index]);
-			$('#image').attr('src', images[index]);
+			$('#image').attr('src', images[index].src);
+			$('.media').fadeIn(500);
 			index = (index == 9) ? 0 : (index + 1);
-			$('.media').fadeIn(300);
 		});
 	});
 });

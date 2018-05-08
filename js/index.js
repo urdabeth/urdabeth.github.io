a01b967abcf2c2d3c2dfea34704d715cf800e0c7
Paolo Carando <paolo.carando@xurpas.com>
2018-05-08 10:22:43 +0800

    Change colors

--- js/index.js
+++ js/index.js
@@ -33,8 +33,10 @@ $(document).ready(function() {
 	var count = 0;
 	$('.message').on('click', function() {
 		$('.message').css('color', '#4cd5ab');
+		$('.message').css('border-color', '#4cd5ab');
 		setTimeout(function() {
 			$('.message').css('color', '#666');
+			$('.message').css('border-color', '#666');
 		}, 100);
 		audio.currentTime = 0;
 		audio.play();

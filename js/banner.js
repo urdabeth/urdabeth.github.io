cbb19c4a0a1ae62ada928940abd499c2098fd646
Paolo Carando <paolo.carando@xurpas.com>
2018-07-05 12:16:05 +0800

    Rotate images, remove when max

--- js/banner.js
+++ js/banner.js
@@ -1,12 +1,51 @@
-window.onload = function() {
-    var body = document.getElementById("body");
-    for (let i = 0; i < 100; i++) {
-        let top = (Math.floor(Math.random() * window.innerHeight) - 50) + "px";
-        let left = (Math.floor(Math.random() * window.innerWidth) - 50) + "px";
-        let img = document.createElement("img");
-        img.src = "assets/images/icon.png";
-        img.style.top = top;
-        img.style.left = left;
-        body.appendChild(img);
+var interval = null;
+var set = true;
+var count = 0;
+
+function setImages() {
+    set = false;
+
+    interval = setInterval(function() {
+        count++;
+
+        if (count > 50) {
+            clearInterval(interval);
+        } else {
+            let top = (Math.floor(Math.random() * window.innerHeight) - 50) + "px";
+            let left = (Math.floor(Math.random() * window.innerWidth) - 50) + "px";
+            let rotate = "rotate(" + (Math.floor(Math.random() * 360)) + "deg)";
+
+            let img = document.createElement("img");
+            img.src = "assets/images/icon.png";
+            img.id = "image" + count;
+            img.style.top = top;
+            img.style.left = left;
+            img.style.transform = rotate;
+
+            document.getElementById("body").appendChild(img);
+        }
+    }, 1000);
+}
+
+function removeImages() {
+    set = true;
+    count = 0;
+    clearInterval(interval);
+
+    let images = document.getElementsByTagName("img");
+    while (images.length > 0) {
+        let id = images[images.length - 1].id;
+        let image =  document.getElementById(id);
+        image.parentNode.removeChild(image);
     }
+}
+
+window.onload = function() {
+    document.getElementById("body").addEventListener("click", function() {
+        if (set) {
+            setImages();
+        } else {
+            removeImages();
+        }
+    });
 }
